d6b0032f6484125548f48d689a39850039f82586
Tanisha Sinha <sinhatanisha918@gmail.com>
2025-05-27 12:16:06 +0530

    updated

--- Pipeline crossing calculation/pipeline_crossing_cal2.py
+++ Pipeline crossing calculation/pipeline_crossing_cal2.py
@@ -1,66 +1,107 @@
 import math
-#1)Input
-
-Steel_Grade = 300
-Pipe_type = "Seamless"
-Pipe_OD = 1016 #in mm
-Pipe_wall_thickness = 10 #in mm
-smys = 195 #in MPa
-Depth_of_cover = 1.5 #in m
-Corrosion_Allowence = 0 #in mm
-Pipe_wall_thickness_inc_CA = 10 #in mm
-Bored_Diameter = 1067 #in mm
-Soil_unit_Weight = 18.9 #kN/mcu.
-Soil_type = "Soft to Medium Clay"
-Mod_of_Soil_reaction = 3.4 #in MPa
+Steel_grade = 330
+Pipe_Type = "Seamless"
+Pipe_Outside_Diameter = 1016 #mm
+Pipe_Wall_Thickness = 10 #mm
+Specified_Minimum_Yield_Strength = 195 #MPa
+Depth_of_Cover = 1.5 #m
+Corrosion_Allowance = 0 #mm
+Pipe_Wall_Thickness_Including_CA = 10 #mm
+Bored_Diameter = 1067 #mm
+Soil_Unit_Weight = 18.9 #kN/m3
+Soil_Type = "Soft_to_medium_clay" 
+Modulus_of_Soil_Reaction = 3.4 #MPa
 Resilient_Modulus = 34 #MPa
-Operating_pressure = 0 #bar
-Impact_factor = 1.5
-Design_factor = 0.72
-Longitudnal_Joint_factor = 1
-installation_temp = 65 #in centigrade
-operating_temp = 40 #in centigrade
-Design_wheel_load_from_single_axle = 53.4 #kN
-Design_wheel_load_from_tandem_axle = 44.5 #kN
-Youngs_mod = 210 #GPa
-Poissons_rat = 0.30
-Coefficient_of_thermal_expansion = 1.44 * 10**-5
+Operating_Pressure = 0 #bar
+Impact_Factor = 1.5
+Design_Factor = 0.72
+Longitudinal_Joint_Factor = 1
+Installation_Temperature = 65
+Operating_Temperature = 40
+Design_Wheel_Load_From_Single_Axle = 53.4 #kN
+Design_Wheel_Load_From_Tandem_Axle = 44.5 #kN
+Youngs_Modulus = 210000 #MPa
+Poissons_Ratio = 0.30
+Coefficient_of_Thermal_Expansion = 1.44*10**(-5)  
+Applied_Design_Surface_Pressure = 0.478 #MPa
+Thickness_to_diameter_ratio = 0.00984 
+Ratio_of_bore_diameter_and_pipe_diameter = 1.05
+Ratio_of_pipe_dept_and_bore_daimeter = 1.406
+Barlow_Stress = 0 #MPa
+# -------------------------------------Circumferential Stress due to Earth Load--------------------------------------------------------------------------------------------------------------
+Earth_Load_Stiffness_Factor = 6330
+Earth_Load_Burial_Factor = 0.47
+Earth_Load_Excavation_Factor = 0.91
+Stress_due_to_Earth_Load = 51.987 #MPa
+# -------------------------------------C)Cyclic Stresses-------------------------------------------------------------------------------------------------------------------------------------
 
+Stiffness_Factor_KHh = 19.8
+Geometry_Factor_GHh = 0.70
+Road_Pavement_Type_Factor = 1
+Road_Axle_Configuration_Factor = 1
+Cyclic_Circumferential_Stress = 9.948
+Stiffness_Factor_KLh = 14.8
+Geometry_Factor_GLh = 0.64
+Cyclic_Longitudinal_Stress = 6.798 #MPa
 #2) Formulae Calculation
-Applied_Design_Surface_Pressure = ((Design_wheel_load_from_tandem_axle)/0.093)/1000
-print("Applied Design surface Pressure:",round(Applied_Design_Surface_Pressure,3),"MPa")
+Pipe_Wall_Thickness_Including_CA = (Pipe_Wall_Thickness - Corrosion_Allowance)
+print("Pipe_Wall_Thickness_Including_CA : " , Pipe_Wall_Thickness_Including_CA , "mm")
 
-thickness_to_diameter_ratio = (Pipe_wall_thickness)/Pipe_OD
-print("Thickness to Diameter Ratio:",round(thickness_to_diameter_ratio,6))
+user_input = input("Consider bored diameter? (yes/no): ").strip().lower()
+consider_bored_diameter = user_input == "yes"
 
-Ratio_of_bore_diameter_and_pipe_diameter = Bored_Diameter/Pipe_OD
-print("ratio of Bored Diameter and pipe Diameter:",round(Ratio_of_bore_diameter_and_pipe_diameter,2))
+Pipe_Outside_Diameter = 1016 #mm  
 
-Ratio_of_pipe_depth_and_bored_diameter = (Depth_of_cover*1000)/Bored_Diameter
-print("Ratio of Pipe Depth and Bored Diameter:",round(Ratio_of_pipe_depth_and_bored_diameter,3))
+if consider_bored_diameter:
+    Bored_Diameter = Pipe_Outside_Diameter + 51
+else:
+    Bored_Diameter = Pipe_Outside_Diameter
 
-S_Hi = (Operating_pressure*(Pipe_OD - Pipe_wall_thickness_inc_CA))/2*Pipe_wall_thickness_inc_CA
+print("Bored_Diameter:", Bored_Diameter, "mm")
+
+
+# Bored_Diameter = (Pipe_Outside_Diameter + 51)
+# print("Bored_Diameter : " , Bored_Diameter , "mm")
+
+Thickness_to_diameter_ratio = (Pipe_Wall_Thickness_Including_CA / Pipe_Outside_Diameter)
+Thickness_to_diameter_ratio = round(Thickness_to_diameter_ratio, 5)
+print("Thickness_to_diameter_ratio : " , Thickness_to_diameter_ratio)
+
+Ratio_of_bore_diameter_and_pipe_diameter = (Bored_Diameter / Pipe_Outside_Diameter)
+Ratio_of_bore_diameter_and_pipe_diameter = round(Ratio_of_bore_diameter_and_pipe_diameter, 2)
+print("Ratio_of_bore_diameter_and_pipe_diameter : " , Ratio_of_bore_diameter_and_pipe_diameter)
+
+Ratio_of_pipe_dept_and_bore_daimeter = (Depth_of_Cover * 1000) / Bored_Diameter
+Ratio_of_pipe_dept_and_bore_daimeter = round(Ratio_of_pipe_dept_and_bore_daimeter, 3)
+print("Ratio_of_pipe_dept_and_bore_daimeter:", Ratio_of_pipe_dept_and_bore_daimeter)
+
+Applied_Design_Surface_Pressure = round(Design_Wheel_Load_From_Tandem_Axle/0.093)/1000
+print("Applied_Design_Surface_Pressure : " , Applied_Design_Surface_Pressure , "MPa")
+
+Barlow_Stress = (Operating_Pressure * (Pipe_Outside_Diameter - Pipe_Wall_Thickness_Including_CA ))/2*Pipe_Wall_Thickness_Including_CA
+print("Borrow_Stress : " , Barlow_Stress , "MPa")
+
+F_E_SMYS = (Design_Factor * Longitudinal_Joint_Factor  * Specified_Minimum_Yield_Strength)
+print("F_E_SMYS : " , F_E_SMYS , "MPa")
 
 #3) CALCULATION
 #------------d) Circumferential Stress due to Internal Pressurization
-Stresses_due_to_Earth_load = 51.987 #S_He
-Cyclic_Circumferential_Stress = 9.948 #S_Hh
-Cyclic_Longitudinal_Stress = 6.798 #S_Lh
-E_s =Youngs_mod*1000 #unit conversion of Young's Mod
 
-S1 = Stresses_due_to_Earth_load + Cyclic_Circumferential_Stress + S_Hi
-S2 = Cyclic_Longitudinal_Stress - E_s*Coefficient_of_thermal_expansion*(operating_temp - installation_temp)+Poissons_rat*(Stresses_due_to_Earth_load+S_Hi)
-print("Circumferencial Stress:",S1,"MPa")
-print("Longitudnal Stress:",S2,"MPa")
+
+Circumferential = Stress_due_to_Earth_Load + Cyclic_Circumferential_Stress + Barlow_Stress
+Longitudinal = Cyclic_Longitudinal_Stress - Youngs_Modulus*Coefficient_of_Thermal_Expansion*(Operating_Temperature - Installation_Temperature)+Poissons_Ratio*(Stress_due_to_Earth_Load+Barlow_Stress)
+Radial_Stress = 0
+print("Circumferencial Stress:",Circumferential,"MPa")
+print("Longitudnal Stress:",Longitudinal,"MPa")
 
 #---------------e) Pricipal Stress
-Radial_stress = 0
 
-effective_stress = math.sqrt(1/2*((S1-S2)**2+(S2-Radial_stress)**2+(Radial_stress-S1)**2))
+
+effective_stress = math.sqrt(1/2*((Circumferential-Longitudinal)**2+(Longitudinal-Radial_Stress)**2+(Radial_Stress-Circumferential)**2))
 print("Effective Stress:",effective_stress,"MPa")
 
 #Criteria Check
-if effective_stress<=smys*Design_factor:
+if effective_stress<=Specified_Minimum_Yield_Strength*Design_Factor:
     print("Allowable")
 else:
     print("Not Allowable!")
@@ -68,14 +109,14 @@ else:
 #-----------------f) Fatigue Check
 Fatigue_endurance_Limit_of_Girth_yield = 82.737 #in MPa
 print("Girth Welds criteria:")
-if Cyclic_Longitudinal_Stress<=Fatigue_endurance_Limit_of_Girth_yield*Design_factor:
+if Cyclic_Longitudinal_Stress<=Fatigue_endurance_Limit_of_Girth_yield*Design_Factor:
     print("Allowable")
 else:
     print("Not Allowable!")
 
 Fatigue_endurance_Limit_of_Longitudinal_Weld = 158.57
 print("Longitutnal Welds check:")
-if Cyclic_Circumferential_Stress<=Fatigue_endurance_Limit_of_Longitudinal_Weld*Design_factor:
+if Cyclic_Circumferential_Stress<=Fatigue_endurance_Limit_of_Longitudinal_Weld*Design_Factor:
     print("Allowable")
 else:
     print("Not Allowable!")
